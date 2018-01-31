7acb55c1f7862baacc1268375e972a0b6f2228f0
Alexey Alekhin <laughedelic@gmail.com>
2018-01-31 23:12:06 +0100

    Made BusySignal types traits (they don't correspond to real JS classes)

--- src/main/scala/atom/ide-ui/busy-signal/BusyMessage.scala
+++ src/main/scala/atom/ide-ui/busy-signal/BusyMessage.scala
@@ -4,12 +4,12 @@ import laughedelic.atom.DisposableLike
 import scala.scalajs.js, js.annotation._
 
 // https://github.com/facebook-atom/atom-ide-ui/blob/master/modules/atom-ide-ui/pkg/atom-ide-busy-signal/lib/types.js
-@js.native @JSImport("atom-ide-busy-signal", "BusyMessage")
+@js.native
 trait BusyMessage extends DisposableLike {
 
-  // You can set/update the title.
+  /** You can set/update the title */
   def setTitle(title: String): Unit = js.native
 
-  // Dispose of the signal when done to make it go away.
+  /** Dispose of the signal when done to make it go away */
   def dispose(): Unit = js.native
 }
