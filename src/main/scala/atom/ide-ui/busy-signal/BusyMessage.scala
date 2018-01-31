bbb2549a94c75a02e3385cfa55b2ffdee9ba1e74
Alexey Alekhin <laughedelic@gmail.com>
2018-01-31 23:10:25 +0100

    Moved DisposableLike to the root package to reuse everywhere

--- src/main/scala/atom/ide-ui/busy-signal/BusyMessage.scala
+++ src/main/scala/atom/ide-ui/busy-signal/BusyMessage.scala
@@ -1,10 +1,11 @@
 package laughedelic.atom.ide.ui.busysignal
 
+import laughedelic.atom.DisposableLike
 import scala.scalajs.js, js.annotation._
 
 // https://github.com/facebook-atom/atom-ide-ui/blob/master/modules/atom-ide-ui/pkg/atom-ide-busy-signal/lib/types.js
 @js.native @JSImport("atom-ide-busy-signal", "BusyMessage")
-class BusyMessage extends js.Object {
+trait BusyMessage extends DisposableLike {
 
   // You can set/update the title.
   def setTitle(title: String): Unit = js.native
