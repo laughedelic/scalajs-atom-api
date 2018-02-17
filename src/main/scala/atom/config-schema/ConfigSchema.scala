0d86e8b9adbd9395b7ca26d3b37e6ea47aaa48d7
Alexey Alekhin <laughedelic@gmail.com>
2018-02-17 03:24:34 +0100

    Automatic settings order initialization

--- src/main/scala/atom/config-schema/ConfigSchema.scala
+++ src/main/scala/atom/config-schema/ConfigSchema.scala
@@ -7,13 +7,20 @@ class ConfigSchema extends js.Object { conf: Singleton =>
 
   def init(prefix: String): ConfigSchema = {
     conf.asInstanceOf[js.Dictionary[js.Dynamic]]
-      .foreach { case (key, value) =>
-        val newPrefix = s"${prefix}.${key}"
-        if (value.isInstanceOf[Setting[_]])
-          value.updateDynamic("label")(newPrefix)
-        else if (value.isInstanceOf[SettingsGroup[_]])
-          value.asInstanceOf[SettingsGroup[ConfigSchema]]
-            .properties.init(newPrefix)
+      .filter {
+        case (_, value: js.Object) =>
+          value.hasOwnProperty("type")
+      }
+      .zipWithIndex
+      .foreach {
+        case ((key, value), index) =>
+          value.updateDynamic("order")(index + 1)
+          val newPrefix = s"${prefix}.${key}"
+          if (value.isInstanceOf[Setting[_]])
+            value.updateDynamic("label")(newPrefix)
+          else if (value.isInstanceOf[SettingsGroup[_]])
+            value.asInstanceOf[SettingsGroup[ConfigSchema]]
+              .properties.init(newPrefix)
       }
     conf
   }
--- src/main/scala/atom/config-schema/SettingsGroup.scala
+++ src/main/scala/atom/config-schema/SettingsGroup.scala
@@ -2,13 +2,14 @@ package laughedelic.atom.config
 
 import scala.scalajs.js
 
-class SettingsGroup[S <: ConfigSchema](schema: S)(
+class SettingsGroup[S <: ConfigSchema](
+  schema: S,
   val title: js.UndefOr[String] = js.undefined,
-  val order: js.UndefOr[Int] = js.undefined,
   val collapsed: js.UndefOr[Boolean] = js.undefined,
 ) extends js.Object {
 
   final val `type`: String = "object"
+  final val order: js.UndefOr[Int] = js.undefined
 
   final val properties: S = schema
 }
